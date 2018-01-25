b8985f207320fb702d3dd90f4727e0986c3540d5
Nicolas M <nicolas.mermoud@heig-vd.ch>
2018-01-25 10:30:56 +0100

    Cercle en fonction du point recherché + changement fond de carte
    Fonctionnalité qui affiche un cercle de 10km de rayon autour du point
    renvoyé par le geocoder.
    Style mapbox://styles/mapbox/outdoors-v9 plus adapté pour randonnée
    (courbes de niveau, chemins pédestres...)

--- js/script.js
+++ js/script.js
@@ -3,16 +3,19 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hvdWJpZG91YXAiLCJhIjoiY2pjcWZwaWZqMTV1YTJ3bWlpZ2kycG0xMiJ9.35GbAs9ZgRbsWhX7WZpf0w';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    style: 'mapbox://styles/mapbox/outdoors-v9', // stylesheet location
     center: [8, 46], // starting position [lng, lat]
     zoom: 7 // starting zoom
 });
 
 /*-------- CONTROLES --------*/
 // Recherche
-map.addControl(new MapboxGeocoder({
-	accessToken: mapboxgl.accessToken
-}), 'top-left');
+// On utilise une variable geocoder pour pouvoir récupérer plus loin les coordonnées du point qui sert à créer le cercle
+var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+});
+map.addControl(geocoder, 'top-left');
+
 // Controle +/-
 map.addControl(new mapboxgl.NavigationControl());
 // Géolocalisation
@@ -38,6 +41,42 @@ map.on('load', function () {
             "icon-image": "lodging-15",
             "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
         }
+    });
+
+    // Coordonnées (vide pour l'instant) du point qui servira de centre pour le cercle
+    map.addSource('single-point', {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": []
+        }
+    });
+
+    // Listen for the `geocoder.input` event that is triggered when a user
+    // makes a selection and add a symbol that matches the result.
+    // Tiré de : https://www.mapbox.com/mapbox-gl-js/example/point-from-geocoder-result/
+    // Calcul du rayon du cercle adapté de : https://stackoverflow.com/a/37794326
+    geocoder.on('result', function(ev) {
+        map.getSource('single-point').setData(ev.result.geometry);
+
+        map.addLayer({
+            "id": "point",
+            "source": "single-point",
+            "type": "circle",
+            "paint": {
+                "circle-radius": {
+                    stops: [
+                        [0, 0],
+                        [20, (10000 / 0.075 / Math.cos( ev.result.geometry.coordinates[0] * Math.PI / 180))]
+                    ],
+                    base: 2
+                },
+                "circle-color": "rgba(255, 255, 255, 0)",
+                "circle-stroke-width": 5,
+                "circle-stroke-color": "#000000",
+                "circle-stroke-opacity": 1
+            }
+        });
     });
 
     //var nbClicks = 0;
